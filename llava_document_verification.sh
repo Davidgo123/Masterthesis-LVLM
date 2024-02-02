28dc9f709a0400862a8ffb3b9b44fba698832b0b
Davidgo123 <david.ernst20@gmail.com>
2024-02-02 16:50:57 +0100

    update script

--- llava_document_verification.sh
+++ llava_document_verification.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-python /nfs/home/ernstd/scripts/document_verification/1_prepare_questions.py &
+python /nfs/home/ernstd/masterthesis_scripts/document_verification/prepare_questions.py &
 pid1=$!
 wait $pid1
 
 # - - - - - - - - - -
 
-python /nfs/home/ernstd/models/instructblip-vicuna-7b/document_verification.py &
+python /nfs/home/ernstd/masterthesis_scripts/document_verification/model_scripts/instructblip_dv.py &
 pid2=$!
 wait $pid2
 
-python /nfs/home/ernstd/models/llava-1.5-7b-hf/document_verification.py &
+python /nfs/home/ernstd/masterthesis_scripts/document_verification/model_scripts/llava-7b_dv.py &
 pid3=$!
 wait $pid3
 
-python /nfs/home/ernstd/models/llava-1.5-13b-hf/document_verification.py &
+python /nfs/home/ernstd/masterthesis_scripts/document_verification/model_scripts/llava-13b_dv.py &
 pid4=$!
 wait $pid4
 
 # - - - - - - - - - -
 
-#python /nfs/home/ernstd/scripts/document_verification/2_analyze_answers.py &
-#pid5=$!
-#wait $pid5
+python /nfs/home/ernstd/masterthesis_scripts/document_verification/analyze_answers.py &
+pid5=$!
+wait $pid5
