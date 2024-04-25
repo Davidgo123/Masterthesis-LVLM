3a14c3a9b8ab0b7e72c7abeea0b49aa97a341383
Davidgo123 <david.ernst20@gmail.com>
2024-04-25 22:36:27 +0200

    update

--- run_shell.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-srun --pty -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 zsh && conda activate transformer
-#srun --pty -w devbox3 -c 12 --mem 32G --gres=gpu:t2080ti:1 zsh && conda activate transformer
