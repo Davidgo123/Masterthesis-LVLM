0591f5a668bc6e705a7866ef9122cf6091588b80
Davidgo123 <david.ernst20@gmail.com>
2024-04-29 01:09:02 +0200

    update

--- utils/batch_fulltest.sh
+++ utils/batch_fulltest.sh
@@ -20,9 +20,9 @@ basePath=(
 # # # # # # # # # # # # # # # # # # # #
 #       01 - without images - DV      #
 # # # # # # # # # # # # # # # # # # # #
-sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/11-DV-MMG.out ${basePath[0]}/run_mmg.sh ${basePath[0]} $createQuestions $runModels
-sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/11-DV-News400.out ${basePath[0]}/run_news400.sh ${basePath[0]} $createQuestions $runModels
-sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/11-DV-Tamperednews.out ${basePath[0]}/run_tamperednews.sh ${basePath[0]} $createQuestions $runModels
+# sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/11-DV-MMG.out ${basePath[0]}/run_mmg.sh ${basePath[0]} $createQuestions $runModels
+# sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/11-DV-News400.out ${basePath[0]}/run_news400.sh ${basePath[0]} $createQuestions $runModels
+# sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/11-DV-Tamperednews.out ${basePath[0]}/run_tamperednews.sh ${basePath[0]} $createQuestions $runModels
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -31,9 +31,9 @@ sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/11-D
 # # # # # # # # # # # # # # # # # # # #
 #       01 - without images - EV      #
 # # # # # # # # # # # # # # # # # # # #
-sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/12-EV-MMG.out ${basePath[1]}/run_mmg.sh ${basePath[1]} $createQuestions $runModels
-sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/12-EV-News400.out ${basePath[1]}/run_news400.sh ${basePath[1]} $createQuestions $runModels
-sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/12-EV-Tamperednews.out ${basePath[1]}/run_tamperednews.sh ${basePath[1]} $createQuestions $runModels
+# sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/12-EV-MMG.out ${basePath[1]}/run_mmg.sh ${basePath[1]} $createQuestions $runModels
+# sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/12-EV-News400.out ${basePath[1]}/run_news400.sh ${basePath[1]} $createQuestions $runModels
+# sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/12-EV-Tamperednews.out ${basePath[1]}/run_tamperednews.sh ${basePath[1]} $createQuestions $runModels
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -42,9 +42,9 @@ sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/12-E
 # # # # # # # # # # # # # # # # # # # # # 
 #       02 - with single image - EV     #
 # # # # # # # # # # # # # # # # # # # # # 
-# sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/211-EV-1x1-News400.out ${basePath[2]}/run_news400.sh ${basePath[2]} $createQuestions $runModels
-# sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/211-EV-1x1-Tamperednews.out ${basePath[2]}/run_tamperednews.sh ${basePath[2]} $createQuestions $runModels
-# sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/212-EV-1xN-Tamperednews.out ${basePath[3]}/run_tamperednews.sh ${basePath[3]} $createQuestions $runModels
+sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/211-EV-1x1-News400.out ${basePath[2]}/run_news400.sh ${basePath[2]} $createQuestions $runModels
+sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/211-EV-1x1-Tamperednews.out ${basePath[2]}/run_tamperednews.sh ${basePath[2]} $createQuestions $runModels
+sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/212-EV-1xN-Tamperednews.out ${basePath[3]}/run_tamperednews.sh ${basePath[3]} $createQuestions $runModels
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -53,6 +53,6 @@ sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/12-E
 # # # # # # # # # # # # # # # # # # # # #
 #       02 - with multi image - EV      #
 # # # # # # # # # # # # # # # # # # # # #
-# sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/221-EV-1x1-News400.out ${basePath[4]}/run_news400.sh ${basePath[4]} $createQuestions $runModels
-# sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/221-EV-1x1-Tamperednews.out ${basePath[4]}/run_tamperednews.sh ${basePath[4]} $createQuestions $runModels
-# sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/222-EV-1xN-Tamperednews.out ${basePath[5]}/run_tamperednews.sh ${basePath[5]} $createQuestions $runModels
+sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/221-EV-1x1-News400.out ${basePath[4]}/run_news400.sh ${basePath[4]} $createQuestions $runModels
+sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/221-EV-1x1-Tamperednews.out ${basePath[4]}/run_tamperednews.sh ${basePath[4]} $createQuestions $runModels
+sbatch -w devbox5 -c 12 --mem 32G --gres=gpu:a3090:1 --output=./output/logs/222-EV-1xN-Tamperednews.out ${basePath[5]}/run_tamperednews.sh ${basePath[5]} $createQuestions $runModels
